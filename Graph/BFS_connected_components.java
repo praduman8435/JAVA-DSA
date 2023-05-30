ad62c2d918453b1ef78f3356b36dddf5c0d57f8a
Praduman Prajapati <tech.praduman@gmail.com>
2023-05-31 02:01:22 +0530

    Added BFS_connected_components

---
+++ Graph/BFS_connected_components.java
@@ -0,0 +1,100 @@
+package Graph;
+
+import java.util.*;
+public class BFS_connected_components {
+        static class Edge{
+            int src;
+            int dest;
+            int wt;
+            public Edge(int s, int d, int w){
+                this.src=s;
+                this.dest=d;
+                this.wt=w;
+            }
+        }
+        static  void createGraph(ArrayList<Edge>[] graph){     // O(V+E)
+            for (int i=0; i<graph.length; i++){
+                graph[i]= new ArrayList<>();
+            }
+
+            graph[0].add(new Edge(0,1,1));
+            graph[0].add(new Edge(0,2,1));
+
+            graph[1].add(new Edge(1,0,1));
+            graph[1].add(new Edge(1,3,1));
+
+            graph[2].add(new Edge(2,0,1));
+            graph[2].add(new Edge(2,4,1));
+
+            graph[3].add(new Edge(3,1,1));
+            graph[3].add(new Edge(3,5,1));
+            graph[3].add(new Edge(3,4,1));
+
+            graph[4].add(new Edge(4,3,1));
+            graph[4].add(new Edge(4,5,1));
+            graph[4].add(new Edge(4,2,1));
+
+            graph[5].add(new Edge(5,6,1));
+            graph[5].add(new Edge(5,4,1));
+            graph[5].add(new Edge(5,3,1));
+
+            graph[6].add(new Edge(6,5,1));
+        }
+
+        public static void bfs(ArrayList<Edge>[] graph){
+            boolean visit[] =  new boolean[graph.length];
+            for (int i=0; i< graph.length; i++){
+                if (!visit[i]){
+                    bfsutil(graph, visit);
+                }
+            }
+        }
+
+        public static void bfsutil(ArrayList<Edge>[] graph, boolean visit[]) {
+            Queue<Integer> q= new LinkedList<>();
+            q.add(0);   //source = 0;
+
+            while (!q.isEmpty()){
+                int curr= q.remove();
+
+                if(!visit[curr]){
+                    System.out.println(curr+" ");
+                    visit[curr]= true;
+                    for(int i=0; i< graph[curr].size(); i++){
+                        Edge e= graph[curr].get(i);
+                        q.add(e.dest);
+                    }
+                }
+            }
+        }
+
+        public static void main(String[] args) {
+        /*
+
+              1----3
+             /     | \
+            0      |  5--6
+             \     | /
+              2----4
+
+        */
+
+            int V=7;
+            ArrayList<Edge> graph[] = new ArrayList[V];
+            createGraph(graph);
+            bfs(graph);
+        }
+    }
+
+/*
+
+output
+0
+1
+2
+3
+4
+5
+6
+
+ */
