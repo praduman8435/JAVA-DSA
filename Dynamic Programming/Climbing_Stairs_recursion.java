63be9217e6985ed3eb99053bfd6e91e41a89f426
Praduman Prajapati <tech.praduman@gmail.com>
2023-06-18 21:48:50 +0530

    Added Climbing_Stairs_recursion

---
+++ Dynamic Programming/Climbing_Stairs_recursion.java
@@ -0,0 +1,31 @@
+package DP;
+import java.util.*;
+
+/*
+
+count ways to reach the nth stair. The person can climb either 1stair or 2stair at a time
+
+*/
+
+public class Climbing_Stairs_recursion {
+    public static int countWays(int n){
+        if (n == 0){
+            return 1;
+        }
+        if (n < 0){
+            return 0;
+        }
+        return countWays(n-1)+countWays(n-2);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(countWays(n));
+    }
+}
+
+/*
+
+output
+8
+
+*/
