7f0d200da223dbc5ba6970683cf42a52d6de71a1
Praduman Prajapati <tech.praduman@gmail.com>
2023-07-06 01:26:17 +0530

    Added N_Queens code

---
+++ Backtracking/N_Queens.java
@@ -0,0 +1,76 @@
+package Backtracking;
+import java.util.*;
+public class N_Queens {
+
+    public static boolean isSafe(char board[][],int row, int col){
+        //vertically up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        //diagonal left up
+        for(int i=row-1,j=col-1;i>=0 && j>=0;i--,j--){
+            if (board[i][j]=='Q'){
+                return false;
+            }
+        }
+        //diagonally right up
+        for (int i=row-1,j=col+1;i>=0&&j< board.length;i--,j++){
+            if (board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void nQueens(char board[][],int row){
+        if(row== board.length){
+            printBoard(board);
+            return;
+        }
+        for (int j=0;j< board.length;j++){
+            if (isSafe(board,row,j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1);
+                board[row][j] = 'x';
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]){
+        System.out.println("-------chess board-------");
+        for (int i=0;i< board.length;i++){
+            for (int j=0;j< board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n= 5;
+        char board[][] = new char [n][n];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                board[i][j]= 'x';
+            }
+        }
+        nQueens(board,0);
+
+    }
+}
+/*
+output
+
+x Q x x
+x x x Q
+Q x x x
+x x Q x
+-------chess board-------
+x x Q x
+Q x x x
+x x x Q
+x Q x x
+
+*/
