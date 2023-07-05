06ee044aac9f61a941e41f1bf7a2b9120a211352
Praduman Prajapati <tech.praduman@gmail.com>
2023-07-06 01:43:11 +0530

    Get_ith_Bit

---
+++ Bit_Manupulation/Get_ith_Bit.java
@@ -0,0 +1,25 @@
+package Bit_Manupulation;
+
+public class Get_ith_Bit {
+
+    public static int getIthBit(int n, int i){
+        int bitMask =1<<i;
+        if((n&bitMask)==0){
+            return 0;
+        }else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(getIthBit(10,2));
+
+    }
+}
+/*
+output
+
+0
+
+*/
